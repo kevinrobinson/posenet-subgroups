a6c64a40839c76a7fefdb30e18a20b7abc040fb8
kevinrobinson <kevin.robinson.0@gmail.com>
2019-03-07 19:19:31 -0500

    Initial experiment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import _ from 'lodash';
+import * as posenet from '@tensorflow-models/posenet';
 import './App.css';
+import Face from './Face';
+import youngWhiteGirl from './searches/youngWhiteGirl.json';
+import preschoolKidWithGlasses from './searches/preschoolKidWithGlasses.json';
+import highSchoolKidWithGlasses from './searches/highSchoolKidWithGlasses.json';
+import youngBlackMan from './searches/youngBlackMan.json';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PoseNet
+          loadingEl={renderLoading()}
+          whenLoaded={net => renderPage(net)}
+        />
+      </header>
+    </div>
+  );
 }
 
-export default App;
+function renderLoading() {
+  return (
+    <div>
+      <div>Loading PoseNet...</div>
+      <img className="App-logo" alt="waiting..." src={logo} />
+    </div>
+  );     
+}
+
+function renderPage(net) {
+  return (
+    <div>
+      <h1 style={{marginBottom: 10}}>What does PoseNet see?</h1>
+      <small>These are some examples of what it sees on images that come up in searches.</small>
+      <small>To learn more, see <a className="App-link" href="https://www.ajlunited.org/gender-shades">ajlunited.org/gender-shades</a></small>
+      {renderSearch(net, youngBlackMan)}
+      {renderSearch(net, youngWhiteGirl)}
+      {renderSearch(net, highSchoolKidWithGlasses)}
+      {renderSearch(net, preschoolKidWithGlasses)}
+      <div style={{margin: 40}}>Try your own experiments!</div>
+    </div>
+  );
+}
+
+
+// Skip images srcs that are empty strings, take only the first few
+function renderSearch(net, search) {
+  const {query, source, date, srcs} = search;
+  return (
+    <div key={query} className="App-search">
+      <div><b>"{query}"</b></div>
+      <small>from {source} on {date}</small>
+      {_.compact(srcs).slice(0, 5).map((src, index) => <Face net={net} key={index} src={src} />)}
+    </div>
+  );
+}
+
+// Loads posenet and calls `whenLoaded` with it when ready.
+function PoseNet({loadingEl, whenLoaded}) {
+  const [net, setNet] = useState(null);
+  useEffect(() => {
+    console.log('useEffect');
+    if (net) return;
+    console.log('load()');
+    posenet.load().then(loadedNet => {
+      console.log('loaded');
+      setNet(loadedNet);
+    });
+  }, [net]);
+
+  if (!net) return loadingEl;
+  return whenLoaded(net);
+}
---
+++ src/Face.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import './Face.css';
+
+// Show an image of a face, and run PoseNet estimate on it and draw an overlay.
+export default class Face extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      busy: false,
+      score: null,
+      error: null,
+      keypoints: null,
+    };
+    this.imageEl = null;
+
+    this.estimate = this.estimate.bind(this);
+    this.startEstimate = this.startEstimate.bind(this);
+  }
+
+  componentDidMount() {
+    this.startEstimate();
+  }
+
+  estimate() {
+    const {net} = this.props;
+    const imageScaleFactor = 0.50;
+    const flipHorizontal = false;
+    const outputStride = 16;
+    return net.estimateSinglePose(this.imageEl, imageScaleFactor, flipHorizontal, outputStride);
+  }
+
+  startEstimate() {
+    this.setState({busy: true});
+    this.estimate()
+      .then(({score, keypoints}) => this.setState({score, keypoints, busy: false}))
+      .catch(error => this.setState({error}));
+  }
+
+  render() {
+    const {keypoints, score, error} = this.state;
+    const {src, alt} = this.props;
+    const altText = alt || 'face';
+
+    return (
+      <div
+        className="Face">
+        <div className="Face-container">
+          <img
+            ref={el => this.imageEl = el}
+            className="Face-image"
+            src={src}
+            height={200}
+            alt={altText}
+            onError={e => {
+              /* Renders a 1x1 white pixel */
+              e.target.src='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+            }}
+          />
+          <div>
+            {keypoints && this.renderKeypoints(keypoints)}
+          </div>
+        </div>
+        <div style={{
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          {score && <div width="50%">{score.toFixed(2)} overall confidence</div>}
+          {error && <div>error! {error.toString()}</div>}
+        </div>
+      </div>
+    );
+  }
+
+  renderKeypoints(keypoints) {
+    const leftEye = _.find(keypoints, { part: 'leftEye' });
+    const rightEye = _.find(keypoints, { part: 'rightEye' });
+    const leftEar = _.find(keypoints, { part: 'leftEar' });
+    const rightEar = _.find(keypoints, { part: 'rightEar' });
+    const nose = _.find(keypoints, { part: 'nose' });
+    const faceKeypoints = [leftEye, rightEye, leftEar, rightEar, nose];
+    const {left, right, bottom, top} = faceBox(keypoints);
+
+    return (
+      <div>
+        <div style={{
+          position: 'absolute',
+          left,
+          top,
+          width: (right - left),
+          height: (bottom - top),
+          borderRadius: 2,
+          background: 'blue',
+          opacity: 0.5
+        }}></div>
+        {faceKeypoints.map(keypoint => (
+          this.renderDot(keypoint, 2, {
+            borderRadius: 4,
+            background: 'white'
+          })
+        ))}
+        {this.renderDot(nose, 5, {
+          borderRadius: 10,
+          background: 'red'
+        })}
+      </div>
+    );
+  }
+
+  renderDot(keypoint, radius, style) {
+    const {x, y} = keypoint.position;
+    const {part} = keypoint;
+    return (
+      <div key={part} style={{
+        position: 'absolute',
+        left: x - radius,
+        top: y - radius,
+        width: radius * 2,
+        height: radius * 2,
+        ...style
+      }}></div>
+    );
+  }
+}
+Face.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string
+};
+
+
+function faceBox(keypoints) {
+  const leftEye = _.find(keypoints, { part: 'leftEye' });
+  const rightEye = _.find(keypoints, { part: 'rightEye' });
+  const leftEar = _.find(keypoints, { part: 'leftEar' });
+  const rightEar = _.find(keypoints, { part: 'rightEar' });
+  const nose = _.find(keypoints, { part: 'nose' });
+  const faceKeypoints = [leftEye, rightEye, leftEar, rightEar, nose];
+  const left = _.min(faceKeypoints.map(keypoint => keypoint.position.x));
+  const right = _.max(faceKeypoints.map(keypoint => keypoint.position.x));
+  const top = _.min(faceKeypoints.map(keypoint => keypoint.position.y));
+  const bottom = _.max(faceKeypoints.map(keypoint => keypoint.position.y));
+  return {left, right, bottom, top};
+}
