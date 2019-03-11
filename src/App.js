fc669dba255c88e82c24186ec380ca2b0fcb98d5
kevinrobinson <kevin.robinson.0@gmail.com>
2019-03-10 20:03:02 -0400

    Add ingredients label

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as posenet from '@tensorflow-models/posenet';
 import './App.css';
 import Spinner from './Spinner';
 import Face from './Face';
+import IngredientsLabel from './IngredientsLabel';
 import YourOwnSearch from './YourOwnSearch';
 import youngWhiteGirl from './searches/youngWhiteGirl.json';
 import preschoolKidWithGlasses from './searches/preschoolKidWithGlasses.json';
@@ -16,6 +17,20 @@ import teenWearingHeadphones from './searches/teenWearingHeadphones.json';
 import blackWomanWearingMakeup from './searches/blackWomanWearingMakeup.json';
 import laughingOldMan from './searches/laughingOldMan.json';
 
+// different searches
+const sets = [
+  blackWomanWearingMakeup,
+  preschoolKidWithGlasses,
+  youngWhiteGirl,
+  manWearingTurban,
+  laughingOldMan,
+  womanWearingEarrings,
+  youngBlackMan,
+  highSchoolKidWithGlasses,
+  toddlerCrying,
+  teenWearingHeadphones
+];
+
 
 export default function App() {
   return (
@@ -23,7 +38,7 @@ export default function App() {
       <header className="App-header">
         <PoseNet
           loadingEl={renderLoading()}
-          whenLoaded={net => <Page net={net} />}
+          whenLoaded={net => <Page net={net} sets={sets} />}
         />
       </header>
     </div>
@@ -39,23 +54,26 @@ function renderLoading() {
   );     
 }
 
-function Page({net}) {
-  // different searches
-  const sets = [
-    blackWomanWearingMakeup,
-    preschoolKidWithGlasses,
-    youngWhiteGirl,
-    manWearingTurban,
-    laughingOldMan,
-    womanWearingEarrings,
-    youngBlackMan,
-    highSchoolKidWithGlasses,
-    toddlerCrying,
-    teenWearingHeadphones
-  ];
+
+// Loads posenet and calls `whenLoaded` with it when ready.
+function PoseNet({loadingEl, whenLoaded}) {
+  const [net, setNet] = useState(null);
+  useEffect(() => {
+    if (net) return;
+    posenet.load().then(loadedNet => {
+      setNet(loadedNet);
+    });
+  }, [net]);
+
+  if (!net) return loadingEl;
+
+  return whenLoaded(net);
+}
+
+
+function Page({net, sets}) {
   const [index, setIndex] = useState(Math.floor(Math.random()*sets.length));
   const set = sets[index];
-  const {query, source, date} = set;
 
   // paging
   const batchSize = 3;
@@ -65,37 +83,11 @@ function Page({net}) {
     <div className="App-page">
       <h1 style={{marginBottom: 10}}>What does PoseNet see?</h1>
       <div><small>These are some examples of what it sees in images that come up from different searches.</small></div>
-      <div className="App-paging">
-        <div className="App-page-arrow App-page-left">
-          <span
-            className="App-link"
-            onClick={() => {
-              setN(batchSize);
-              setIndex(index < 0 ? sets.length - 1 : index - 1);
-            }}
-            role="img"
-            aria-label="left">
-            ◄
-          </span>
-        </div>
-        <div className="App-page-search">
-          <div><b>"{query}"</b></div>
-          <small>from {source} on {date}</small>
-        </div>
-        <div className="App-page-arrow App-page-right">
-          <span
-            className="App-link"
-            onClick={() => {
-              setN(batchSize);
-              setIndex(index >= sets.length ? 0 : index + 1);
-            }}
-            role="img"
-            aria-label="right">
-            ►
-          </span>
-        </div>
-      </div>
 
+      <Paging
+        setIndex={setIndex}
+        set={set}
+      />
       <StoredSearch
         net={net}
         set={set}
@@ -108,6 +100,75 @@ function Page({net}) {
         <YourOwnSearch renderImage={src => <Face net={net} src={src} />} />
       </div>
 
+      <LearnMore />
+
+      <div className="App-ingredients"><ProjectIngredients /></div>
+    </div>
+  );
+}
+
+
+function Paging(props = {}) {
+  const {set, index, setIndex, batchSize, setN} = props;
+  const {query, source, date} = set;
+
+  return (
+    <div className="App-paging">
+      <div className="App-page-arrow App-page-left">
+        <span
+          className="App-link"
+          onClick={() => {
+            setN(batchSize);
+            setIndex(index < 0 ? sets.length - 1 : index - 1);
+          }}
+          role="img"
+          aria-label="left">
+          ◄
+        </span>
+      </div>
+      <div className="App-page-search">
+        <div><b>"{query}"</b></div>
+        <small>from {source} on {date}</small>
+      </div>
+      <div className="App-page-arrow App-page-right">
+        <span
+          className="App-link"
+          onClick={() => {
+            setN(batchSize);
+            setIndex(index >= sets.length ? 0 : index + 1);
+          }}
+          role="img"
+          aria-label="right">
+          ►
+        </span>
+      </div>
+    </div>
+  );
+}
+
+
+// Skip images srcs that are empty strings, take only the first few
+function StoredSearch({net, set, n, onMore}) {
+  const {query, srcs} = set;
+  return (
+    <div key={query} className="App-search">
+      {_.compact(srcs).slice(0, n).map((src, index) => <Face net={net} key={index} src={src} />)}
+      <small>
+        <div
+          className="App-link"
+          style={{marginTop: 10}}
+          onClick={e => e.preventDefault() || onMore()}>
+          see more
+        </div>
+      </small>
+    </div>
+  );
+}
+
+
+function LearnMore() {
+  return (
+    <div>
       <div className="App-learn-more-section-title">Learn more</div>
       <div className="App-learn-more">
         <div className="App-learn-more-panel">
@@ -138,38 +199,11 @@ function Page({net}) {
   );
 }
 
-
-// Skip images srcs that are empty strings, take only the first few
-function StoredSearch({net, set, n, onMore}) {
-  const {query, srcs} = set;
-  return (
-    <div key={query} className="App-search">
-      {_.compact(srcs).slice(0, n).map((src, index) => <Face net={net} key={index} src={src} />)}
-      <small>
-        <div
-          className="App-link"
-          style={{marginTop: 10}}
-          onClick={e => e.preventDefault() || onMore()}>
-          see more
-        </div>
-      </small>
-    </div>
-  );
+function ProjectIngredients() {
+  return <IngredientsLabel
+    dataSets={<div><a target="_blank" rel="noopener noreferrer" href="http://www.image-net.org/">ImageNet</a></div>}
+    preTrainedModels={<div><a target="_blank" rel="noopener noreferrer"  href="https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/classification/2">MobileNet</a> by Google</div>}
+    architectures={<div><a target="_blank" rel="noopener noreferrer" href="https://github.com/tensorflow/tfjs-models/tree/master/posenet">PoseNet</a> by <a target="_blank" rel="noopener noreferrer" href="https://www.danioved.com/">Dan Oved</a></div>}
+    tunings={<div><a target="_blank" rel="noopener noreferrer" href="https://github.com/kevinrobinson/posenet-subgroups/blob/master/src/Face.js#L27">Face.js</a> by <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/krob/">Kevin Robinson</a></div>}
+  />;
 }
-
-// Loads posenet and calls `whenLoaded` with it when ready.
-function PoseNet({loadingEl, whenLoaded}) {
-  const [net, setNet] = useState(null);
-  useEffect(() => {
-    if (net) return;
-    posenet.load().then(loadedNet => {
-      setNet(loadedNet);
-    });
-  }, [net]);
-
-  if (!net) return loadingEl;
-
-  return whenLoaded(net);
-}
-
-
